c48c25c10aa13bfe90fd68f8b611f681eee945f6
AlexanderC <alexander.cucer@flosites.com>
2014-01-23 18:33:03 +0200

    added more verbose passwd ex. in config file

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ error_reporting(-1);
 define("DB_HOST", "localhost");
 define("DB_NAME", "jabberd2");
 define("DB_USER", "jabberd2");
-define("DB_PASS", "13mSieeKVl3E6tYjgYVb");
+define("DB_PASS", "secret");
