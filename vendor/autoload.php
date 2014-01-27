90b6be659d24fea0a33520db1dcdaf43d9b541c3
AlexanderC <alexander.cucer@flosites.com>
2014-01-27 17:26:50 +0200

    changed composer dependency xmpp to a real one

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite9fb893915e7bfd34dcad75f4a860d6d::getLoader();
+return ComposerAutoloaderInit1029670d986955a5d370bedb225e8b2a::getLoader();
