1ddef8a6cf2aafbb25b83c615e773a1512cc7fa7
AlexanderC <alexander.cucer@flosites.com>
2014-01-27 13:22:32 +0200

    edit config file values

--- app/config.php
+++ app/config.php
@@ -8,10 +8,10 @@ return array(
         'pass' => '' // database password
     ),
     'xmpp' => array(
-        'address' => 'tcp://chat.flosites.com:5222', // xmpp server address
-        'username' => 'ross.tanner', // xmpp admin user username
-        'password' => 'WQdm7gQU9oeGUC3GnotT', // xmpp admin user password
-        'alias' => 'Ross Tanner', // xmpp admin user alias (used as groupchat name)
+        'address' => 'tcp://chat.example.com:5222', // xmpp server address
+        'username' => 'john.doe', // xmpp admin user username
+        'password' => 'secret', // xmpp admin user password
+        'alias' => 'Doe John', // xmpp admin user alias (used as groupchat name)
         'mucdir' => '/var/spool/mu-conference/rooms' // Mu Conference Jabberd2 Plugin rooms directory
     ),
     'log_dir' => __DIR__ . "/../logs"
