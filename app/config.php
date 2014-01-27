c443e956dedf60e5d787505051718a04bc123c54
AlexanderC <alexander.cucer@flosites.com>
2014-01-27 13:17:38 +0200

    add some config comments

--- app/config.php
+++ app/config.php
@@ -2,17 +2,17 @@
 
 return array(
     'db' => array(
-        'host' => 'localhost',
-        'name' => 'jabberd2',
-        'user' => 'root',
-        'pass' => ''
+        'host' => 'localhost', // database host
+        'name' => 'jabberd2', // database name
+        'user' => 'root', // database user
+        'pass' => '' // database password
     ),
     'xmpp' => array(
-        'address' => 'tcp://chat.flosites.com:5222',
-        'username' => 'ross.tanner',
-        'password' => 'WQdm7gQU9oeGUC3GnotT',
-        'alias' => 'Ross Tanner',
-        'mucdir' => __DIR__ . '/../tmp/rooms' // mu-conference folder
+        'address' => 'tcp://chat.flosites.com:5222', // xmpp server address
+        'username' => 'ross.tanner', // xmpp admin user username
+        'password' => 'WQdm7gQU9oeGUC3GnotT', // xmpp admin user password
+        'alias' => 'Ross Tanner', // xmpp admin user alias (used as groupchat name)
+        'mucdir' => '/var/spool/mu-conference/rooms' // Mu Conference Jabberd2 Plugin rooms directory
     ),
     'log_dir' => __DIR__ . "/../logs"
 );
